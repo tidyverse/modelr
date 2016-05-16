fbf98c8f3d7c70d2c54d767c2ef9250537416282
hadley <h.wickham@gmail.com>
2016-05-16 09:07:14 -0500

    by and pretty arguments to seq_range()
    * by allows you to specify interval width
    * pretty makes human sequences

--- R/seq_range.R
+++ R/seq_range.R
@@ -1,20 +1,55 @@
 #' Generate a sequence over the range of a vector
 #'
 #' @param x A numeric vector
-#' @param n Length of sequence to generate
+#' @param n,by Specify the output sequence either by supplying the
+#'   length of the sequence with \code{n}, or the spacing between value
+#'   with \code{by}. Specifying both is an error.
+#'
+#'   I recommend that you name these arguments in order to make it clear to
+#'   the reader.
+#' @param pretty If \code{TRUE}, will generate a pretty sequence. If \code{n}
+#'   is supplied, this will use \code{\link{pretty}()} instead of
+#'   \code{\link{seq}()}. If \code{by} is supplied, it will round the first
+#'   value to a multiple of \code{by}.
 #' @param trim Optionally, trim values off the tails.
 #'   \code{trim / 2 * length(x)} values are removed from each tail.
 #' @export
 #' @examples
 #' x <- rcauchy(100)
-#' seq_range(x, 10)
-#' seq_range(x, 10, trim = 0.1)
-seq_range <- function(x, n, trim = NULL) {
+#' seq_range(x, n = 10)
+#' seq_range(x, n = 10, trim = 0.1)
+#' seq_range(x, by = 1, trim = 0.1)
+#'
+#' # Make pretty sequences
+#' y <- runif(100)
+#' seq_range(y, n = 10)
+#' seq_range(y, n = 10, pretty = TRUE)
+#'
+#' seq_range(y, by = 0.1)
+#' seq_range(y, by = 0.1, pretty = TRUE)
+seq_range <- function(x, n, by, trim = NULL, pretty = FALSE) {
+  if (!missing(n) && !missing(by)) {
+    stop("May only specify one of `n` and `by`", call. = FALSE)
+  }
+
   if (!is.null(trim)) {
     rng <- stats::quantile(x, c(trim / 2, 1 - trim / 2), na.rm = TRUE)
   } else {
     rng <- range(x, na.rm = TRUE)
   }
 
-  seq(rng[1], rng[2], length = n)
+  if (missing(by)) {
+    if (pretty) {
+      pretty(rng, n)
+    } else {
+      seq(rng[1], rng[2], length.out = n)
+    }
+  } else {
+    if (pretty) {
+      rng[1] <- floor(rng[1] / by) * by
+      rng[2] <- ceiling(rng[2] / by) * by
+    }
+    seq(rng[1], rng[2], by = by)
+  }
+
 }
