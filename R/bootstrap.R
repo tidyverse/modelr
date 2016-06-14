81234d69c82b734b85b9f923a4933990ed541147
hadley <h.wickham@gmail.com>
2016-06-14 07:58:18 -0500

    Implement bootstrap

---
+++ R/bootstrap.R
@@ -0,0 +1,17 @@
+#' Generate a boostrap replicate
+#'
+#' @param data A data frame
+#' @export
+#' @examples
+#' coef(lm(mpg ~ wt, data = bootstrap(mtcars)))
+#'
+#' library(purrr)
+#' bootstraps <- rerun(100, bootstrap(mtcars))
+#' models <- map(bootstraps, ~ lm(mpg ~ wt, data = .))
+#' tidied <- map_df(models, broom::tidy, .id = "id")
+#'
+#' hist(subset(tidied, term == "wt")$estimate)
+#' hist(subset(tidied, term == "(Intercept)")$estimate)
+bootstrap <- function(data) {
+  data[sample(nrow(data), replace = TRUE), , drop = FALSE]
+}
