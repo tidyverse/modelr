dc59748b676afb362d1e78c735dd73590573b4b1
Peter Menzel <pmenzel@gmail.com>
2019-01-29 22:33:55 +0100

    Pass through type parameter to stats::predict() (#74)
    Fixes #34

--- R/predictions.R
+++ R/predictions.R
@@ -1,17 +1,19 @@
 #' Add predictions to a data frame
 #'
 #' @param data A data frame used to generate the predictions.
-#' @param model,var `add_predictions` takes a single `model`; the
-#'   output column will be called `pred`
+#' @param model `add_predictions` takes a single `model`;
+#' @param var The name of the output column, default value is `pred`
+#' @param type Prediction type, passed on to `stats::predict()`. Consult
+#'   `predict()` documentation for given `model` to determine valid values.
 #' @param ... `gather_predictions` and `spread_predictions` take
 #'   multiple models. The name will be taken from either the argument
 #'   name of the name of the model.
 #' @param .pred,.model The variable names used by `gather_predictions`.
 #' @return A data frame. `add_prediction` adds a single new column,
-#'   `.pred`, to the input `data`. `spread_predictions` adds
-#'   one column for each model. `gather_predictions` adds two columns
-#'   `.model` and `.pred`, and repeats the input rows for
-#'   each model.
+#'   with default name `pred`, to the input `data`.
+#'   `spread_predictions` adds one column for each model. `gather_predictions`
+#'   adds two columns `.model` and `.pred`, and repeats the input rows for each
+#'   model.
 #' @export
 #' @examples
 #' df <- tibble::data_frame(
@@ -27,27 +29,28 @@
 #' m2 <- lm(y ~ poly(x, 2), data = df)
 #' grid %>% spread_predictions(m1, m2)
 #' grid %>% gather_predictions(m1, m2)
-add_predictions <- function(data, model, var = "pred") {
-  data[[var]] <- stats::predict(model, data)
+add_predictions <- function(data, model, var = "pred", type = NULL) {
+  data[[var]] <- stats::predict(model, data, type = type)
   data
 }
 
 #' @rdname add_predictions
 #' @export
-spread_predictions <- function(data, ...) {
+spread_predictions <- function(data, ..., type = NULL) {
   models <- tibble::lst(...)
   for (nm in names(models)) {
-    data[[nm]] <- stats::predict(models[[nm]], data)
+    data[[nm]] <- stats::predict(models[[nm]], data, type = type)
   }
   data
 }
 
 #' @rdname add_predictions
 #' @export
-gather_predictions <- function(data, ..., .pred = "pred", .model = "model") {
+gather_predictions <- function(data, ..., .pred = "pred", .model = "model", type = NULL) {
+
   models <- tibble::lst(...)
 
-  df <- purrr::map2(models, .pred, add_predictions, data = data)
+  df <- purrr::map2(models, .pred, add_predictions, data = data, type = type)
   names(df) <- names(models)
 
   dplyr::bind_rows(df, .id = .model)
